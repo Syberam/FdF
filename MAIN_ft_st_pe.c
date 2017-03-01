61ad91d8ca53fdfae79e0febd1713c1b719e849f
Syberam <sbonnefo@student.42.fr>
2017-03-01 19:57:41 +0100

    main to test

--- MAIN_ft_st_pe.c
+++ MAIN_ft_st_pe.c
@@ -1,22 +1,20 @@
-#include "src/libft/libft.h"
-
-t_peak	*ft_stock_peaks(char *file_path);
-
-void	*ft_seg_to_img(t_peak p1, t_peak p2);
+#include "src/fdf.h"
 
 int		main(int argc, char **argv)
 {
+	t_mlx	*mlx;
 	t_peak	*current;
 	t_peak	*start;
 	void	*img;
 	t_mlx	*param;
 
-	param = (t_mlx)ft_memalloc(sizeof(t_mlx));
+	param = (t_mlx *)ft_memalloc(sizeof(t_mlx));
 	mlx->mlx = mlx_init();
 	mlx->win = mlx_new_window(param->mlx, 800, 800, "segment");
 	if (argc == 0)
 		return (0);
 	start = ft_stock_peaks(argv[1]);
+	ft_peaks_to_plan(start);
 	current = start;
 	while (current->next)
 	{
@@ -28,7 +26,7 @@ int		main(int argc, char **argv)
 		ft_putchar('\n');
 		current = current->next;
 	}
-	img = ft_seg_to_img(start, current);
-	ft_put_image_to_window(param->mlx, param->win, img, 200, 200);
+	img = ft_putpeaks(start, param);
+	mlx_put_image_to_window(param->mlx, param->win, img, start->w, start->h);
 	return (0);
 }
