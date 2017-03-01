5cf12181ffa19056ee67f17f2f3a6805ea263f6b
Syberam <sbonnefo@student.42.fr>
2017-03-01 19:58:16 +0100

    fct to put a peak in the plan

---
+++ ft_peaks_to_plan.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_peaks_to_plan.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbonnefo <sbonnefo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/01 12:42:46 by sbonnefo          #+#    #+#             */
+/*   Updated: 2017/03/01 16:42:37 by sbonnefo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "src/fdf.h"
+
+void			ft_img_size(t_peak *peak)
+{
+		t_peak	*current;
+		int		w;
+		int		h;
+
+		current = peak;
+		while (peak->next)
+		{
+			w = peak->x;
+			h = peak->y;
+			peak = peak->next;
+		}
+		current = peak;
+		while(peak)
+		{
+			peak->w = w;
+			peak->w = w;
+			peak = peak->next;
+		}
+
+}
+
+void		ft_peaks_to_plan(t_peak *peak)
+{
+	int		ox;
+	int		oy;
+
+	ft_img_size(peak);
+	ox = (peak->w) / 2; 
+	oy = (peak->h) / 2; 
+	while (peak)
+	{
+		peak->xx = ox - (peak->y * 10) + (peak->x * 10);
+		peak->yy = oy + (peak->y * 10) - (10 * peak->z);
+		peak = peak->next;
+	}
+}
