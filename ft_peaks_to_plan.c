75565d8fd5334e0c21c6d76f0cc32f390ff1ebf1
Syberam <sbonnefo@student.42.fr>
2017-03-08 20:10:22 +0100

    lol

--- ft_peaks_to_plan.c
+++ ft_peaks_to_plan.c
@@ -6,7 +6,7 @@
 /*   By: sbonnefo <sbonnefo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/01 12:42:46 by sbonnefo          #+#    #+#             */
-/*   Updated: 2017/03/08 19:06:33 by sbonnefo         ###   ########.fr       */
+/*   Updated: 2017/03/08 19:34:56 by sbonnefo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,23 @@ void			ft_img_size(t_peak *peak)
 		int		w;
 		int		h;
 
+		w = 0;
+		h = 0;
 		current = peak;
-		while (peak->next)
+		while (current->next)
 		{
-			w = peak->x;
-			h = peak->y;
-			peak = peak->next;
+			if (current->xx > w)
+				w = current->xx;
+			if (current->yy > h)
+				h = current->yy;
+			current = current->next;
 		}
 		current = peak;
-		while(peak)
+		while(current)
 		{
-			peak->w = w;
-			peak->w = w;
-			peak = peak->next;
+			current->w = w;
+			current->h = h;
+			current = current->next;
 		}
 
 }
@@ -39,14 +43,16 @@ void		ft_peaks_to_plan(t_peak *peak)
 {
 	int		ox;
 	int		oy;
+	t_peak	*start;
 
+	start = peak;
 	ox = (peak->w) / 2; 
 	oy = (peak->h) / 2; 
 	while (peak)
 	{
-		peak->xx = ox - (peak->y * 10) + (peak->x * 10);
-		peak->yy = oy + (peak->y * 10) - (10 * peak->z);
+		peak->xx = ox - (peak->y * 30) + (peak->x * 30);
+		peak->yy = oy + (peak->y * 30) - (30 * peak->z);
 		peak = peak->next;
 	}
-	ft_img_size(peak);
+	ft_img_size(start);
 }
