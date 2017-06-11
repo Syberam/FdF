598d715e099ee1bd27ce5e133decd7c180d26074
Syberam <sbonnefo@student.42.fr>
2017-06-12 00:58:08 +0200

    lol

--- src/include/fdf_keys.h
+++ src/include/fdf_keys.h
@@ -14,26 +14,26 @@
 # define FDF_KEYS_H
 
 # define KEY_ESC	53
-# define KEY_UPL	12
-# define KEY_UP		13
-# define KEY_UPR	14
-# define KEY_L		0
-# define KEY_C		1
-# define KEY_R		2
-# define KEY_DL		6
-# define KEY_DR		8
-# define KEY_DOWN	7
-# define KEY_ROT_R	123
-# define KEY_ROT_L	125
-# define KEY_ROT_D	126
-# define KEY_ROT_U	124
-# define KEY_ZP		24
-# define KEY_ZM		27
-# define KEY_ZPP_U	33
-# define KEY_ZPP_D	30
-# define KEY_HUP	28
-# define KEY_HDO	44
-# define KEY_HINIT	34
-# define KEY_RINIT	29
+# define KEY_UPL	89
+# define KEY_UP		91
+# define KEY_UPR	92
+# define KEY_L		86
+# define KEY_C		87
+# define KEY_R		88
+# define KEY_DL		83
+# define KEY_DR		85
+# define KEY_DOWN	84
+# define KEY_ROT_R	124
+# define KEY_ROT_L	123
+# define KEY_ROT_D	125
+# define KEY_ROT_U	126
+# define KEY_ZP		69
+# define KEY_ZM		78
+# define KEY_ZPP_U	47
+# define KEY_ZPP_D	43
+# define KEY_HUP	75
+# define KEY_HDO	67
+# define KEY_HINIT	81
+# define KEY_RINIT	82
 
 #endif
