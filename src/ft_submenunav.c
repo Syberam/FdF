621c8ec2db1a2a44568efbb1d377e516d70f7e1e
Syberam <sylvainbonnefon@hotmail.com>
2017-06-21 00:02:00 +0200

    delete buttons dir

--- src/ft_submenunav.c
+++ src/ft_submenunav.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sbonnefo <sbonnefo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/06/15 21:42:03 by sbonnefo          #+#    #+#             */
-/*   Updated: 2017/06/15 21:42:07 by sbonnefo         ###   ########.fr       */
+/*   Created: 2017/06/15 21:42:28 by sbonnefo          #+#    #+#             */
+/*   Updated: 2017/06/15 21:57:49 by sbonnefo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void		ft_submenunav(t_mlx *param)
 {
-	mlx_string_put(param->mlx, param->win, 100, 40, 0x00FFFFFF, " 7  8  9");
-	mlx_string_put(param->mlx, param->win, 100, 70, 0x00FFFFFF, " 4  5  6");
-	mlx_string_put(param->mlx, param->win, 100, 100, 0x00FFFFFF, " 1  2  3");
+	mlx_string_put(param->mlx, param->win, 100, 40, 0x00FFFFFF, " Q  W  E");
+	mlx_string_put(param->mlx, param->win, 100, 70, 0x00FFFFFF, " A  S  D");
+	mlx_string_put(param->mlx, param->win, 100, 100, 0x00FFFFFF, " Z  X  C");
 }
--- src/include/fdf_keys.h
+++ src/include/fdf_keys.h
@@ -6,7 +6,7 @@
 /*   By: sbonnefo <sbonnefo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/17 18:30:34 by sbonnefo          #+#    #+#             */
-/*   Updated: 2017/06/15 21:53:34 by sbonnefo         ###   ########.fr       */
+/*   Updated: 2017/06/09 01:03:12 by sbonnefo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,26 @@
 # define FDF_KEYS_H
 
 # define KEY_ESC	53
-# define KEY_UPL	89
-# define KEY_UP		91
-# define KEY_UPR	92
-# define KEY_L		86
-# define KEY_C		87
-# define KEY_R		88
-# define KEY_DL		83
-# define KEY_DR		85
-# define KEY_DOWN	84
+# define KEY_UPL	12
+# define KEY_UP		13
+# define KEY_UPR	14
+# define KEY_L		0
+# define KEY_C		1
+# define KEY_R		2
+# define KEY_DL		6
+# define KEY_DR		8
+# define KEY_DOWN	7
 # define KEY_ROT_R	123
-# define KEY_ROT_L	126
-# define KEY_ROT_D	125
+# define KEY_ROT_L	125
+# define KEY_ROT_D	126
 # define KEY_ROT_U	124
-# define KEY_ZP		69
-# define KEY_ZM		78
-# define KEY_ZPP_U	121
-# define KEY_ZPP_D	116
-# define KEY_HUP	75
-# define KEY_HDO	67
-# define KEY_HINIT	81
-# define KEY_RINIT	82
+# define KEY_ZP		24
+# define KEY_ZM		27
+# define KEY_ZPP_U	33
+# define KEY_ZPP_D	30
+# define KEY_HUP	28
+# define KEY_HDO	44
+# define KEY_HINIT	34
+# define KEY_RINIT	29
 
 #endif
